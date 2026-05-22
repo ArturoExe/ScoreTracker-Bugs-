5542e69eb7acd6bcf4eb3d95f745f754deb7e9a7
Arturo <amarturin15@gmail.com>
2026-05-22 08:27:19 -0700

    Initial Commit

---
+++ ScoreTracker(Bugs)/ScoreTracker_Bugs_App.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreTracker_Bugs_App.swift
+//  ScoreTracker(Bugs)
+//
+//  Created by Arturo Martinez on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct ScoreTracker_Bugs_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
